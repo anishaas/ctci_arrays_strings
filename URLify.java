f5eed5a7b02f443e07fe28c0d46a66d6f889a8c2
Anisha <anisha.srivastava123@gmail.com>
2022-05-06 10:55:10 -0500

    1.3) initial idea, stuck at replacing with chars

---
+++ URLify.java
@@ -0,0 +1,19 @@
+public class URLify {
+
+    // IN PROGRESS - initial thought (iterative approach, replace at detection)
+    static String toURL(String s) {
+        String replacementString = "%20";
+        // iterate through each char in string
+        for(int i=0; i<s.length();i++) {
+            // find occurrence of a space
+            if(s.charAt(i) == ' '){
+                // copy replacement string char by char to this position
+            }
+        }
+        return s;
+    }
+
+    public static void main(String []args){
+
+     }
+}
